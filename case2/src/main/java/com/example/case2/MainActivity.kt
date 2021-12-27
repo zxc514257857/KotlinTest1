bbb0e342c3c47e080a50ad1e6cc77df763b0e6d8
zhanghongrui <developerzhr@gmail.com>
2021-12-27 22:52:51 +0800

    测试分支

--- case2/src/main/java/com/example/case2/MainActivity.kt
+++ case2/src/main/java/com/example/case2/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
     private var isClick: Boolean = false
 
-    // 我在master分支中开发内容
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
